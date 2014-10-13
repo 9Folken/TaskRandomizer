28bb01871926d54ae332f4669cf99c789eb0fe0f
9Folken <dartfolken@inbox.ru>
2014-10-13 12:13:18 +0900

    List

--- TaskRandomizer/src/gui/MainFrame.java
+++ TaskRandomizer/src/gui/MainFrame.java
@@ -3,16 +3,22 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 
 	public MainFrame(String title) {
 		super(title);
@@ -24,30 +30,60 @@ public class MainFrame extends JFrame {
 		JPanel RightPanel = new JPanel(new FlowLayout());
 		JPanel FooterPanel = new JPanel(new FlowLayout());
 		
-		MainPanel.setBackground(Color.BLUE);
+		MainFrame.this.add(MainPanel);
+		
+		MainPanel.setBackground(Color.GRAY);
+		HeaderPanel.setBackground(Color.CYAN);
+		CenterPanel.setBackground(Color.LIGHT_GRAY);
+		LeftPanel.setBackground(Color.RED);
+		RightPanel.setBackground(Color.WHITE);
+		FooterPanel.setBackground(Color.BLACK);
+		
+		MainPanel.add(HeaderPanel,BorderLayout.NORTH);
+		MainPanel.add(CenterPanel,BorderLayout.CENTER);
+		MainPanel.add(LeftPanel,BorderLayout.WEST);
+		MainPanel.add(RightPanel,BorderLayout.EAST);
+		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
+		
+		DefaultListModel LocationListModel = new DefaultListModel();
+		final JList LocationList = new JList(LocationListModel);
+		LocationListModel.addElement("111");
+		LocationListModel.addElement("112");
+		LocationListModel.addElement("113");
+		
+		JTextField text = new JTextField(10); 
 		
 		
 		
 		JButton btnAddLocation = new JButton("Add Location");
-		btnAddLocation.setSize(5, 2);
-		MainFrame.this.add(MainPanel);	
-		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
-		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
-		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
-		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
-		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
+		btnAddLocation.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				 
+				LocationListModel.addElement(text.getText());
+				
+			}
+		});
+		JButton btnAddAction = new JButton("  Add Action  ");
 		
 		
 		
+		FooterPanel.add(btnAddLocation);
+		FooterPanel.add(btnAddAction);
+		CenterPanel.add(LocationList);
+		HeaderPanel.add(text);
 			
 	}
 	
 	
 		
+			
+		}
 		
 		
 		
 		
 	
 
-}
+
