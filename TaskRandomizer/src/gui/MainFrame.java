1e7edc3139e6187f79ac3aa7155235ddbef8d5d7
Folken <paulbykow@list.ru>
2014-10-14 08:04:20 +0900

    // TEST

--- TaskRandomizer/src/gui/MainFrame.java
+++ TaskRandomizer/src/gui/MainFrame.java
@@ -1,82 +1,30 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public static String[] array;  
 
 	public MainFrame(String title) {
+		
 		super(title);
-		
-		JPanel MainPanel = new JPanel(new BorderLayout());
-		JPanel HeaderPanel = new JPanel(new FlowLayout());
-		JPanel CenterPanel = new JPanel(new FlowLayout());
-		JPanel LeftPanel = new JPanel(new FlowLayout());
-		JPanel RightPanel = new JPanel(new FlowLayout());
-		JPanel FooterPanel = new JPanel(new FlowLayout());
-		
-		DefaultListModel LocationListModel = new DefaultListModel();
-		JList LocationList = new JList(LocationListModel);
-		
-        JTextField text = new JTextField(10); 
+		createGUI();
+				
+	}
+	
+	private void createGUI(){
 		
 		JButton btnAddLocation = new JButton("Add Location");
-		JButton btnAddAction = new JButton("  Add Action  ");
-		JButton btnDeleteLocation = new JButton("  Delete location  ");
+		btnAddLocation.setBackground(Color.PINK);
+		getContentPane().add(btnAddLocation);
 		
-		Color color = new Color(232, 115, 169);
-		
-		MainFrame.this.add(MainPanel);
-		
-		MainPanel.setBackground(color);
-		HeaderPanel.setBackground(color);
-		CenterPanel.setBackground(color);
-		LeftPanel.setBackground(color);
-		RightPanel.setBackground(color);
-		FooterPanel.setBackground(color);
-		
-		MainPanel.add(HeaderPanel,BorderLayout.NORTH);
-		MainPanel.add(CenterPanel,BorderLayout.CENTER);
-		MainPanel.add(LeftPanel,BorderLayout.WEST);
-		MainPanel.add(RightPanel,BorderLayout.EAST);
-		MainPanel.add(FooterPanel,BorderLayout.SOUTH);
-		
-		FooterPanel.add(btnAddLocation);
-		FooterPanel.add(btnAddAction);
-		FooterPanel.add(btnDeleteLocation);
-		LeftPanel.add(LocationList);
-		HeaderPanel.add(text);
-		text.setText("enter the text");
-		btnAddLocation.addActionListener(new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-		LocationListModel.addElement(text.getText());
-			}
-		});
-		btnDeleteLocation.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				LocationListModel.remove(LocationList.getSelectedIndex());
-				
-			}
-		});
-			
 	}
-			}
-		
+
+}
+// TEST
